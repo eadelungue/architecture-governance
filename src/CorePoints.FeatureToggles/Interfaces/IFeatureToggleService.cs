9397bb16ea64ed4d7878f56e609a8034177a61c4
Ewerton Leite Adelungue <ewerton.leite@outlook.com>
2026-07-25 23:29:40 -0300

    refactor: move shared libraries (Caching, FeatureToggles, Resilience) to corepoints-shared-libraries repo

--- src/CorePoints.FeatureToggles/Interfaces/IFeatureToggleService.cs
+++
@@ -1,6 +0,0 @@
-namespace CorePoints.FeatureToggles.Interfaces;
-
-public interface IFeatureToggleService
-{
-    Task<bool> IsEnabledAsync(string flagName, string? userId = null, string? group = null, CancellationToken cancellationToken = default);
-}
--- src/CorePoints.FeatureToggles/Models/FeatureToggleOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace CorePoints.FeatureToggles.Models;
-
-public sealed class FeatureToggleOptions
-{
-    public TimeSpan CacheTtl { get; init; } = TimeSpan.FromSeconds(60);
-    public string ConnectionString { get; init; } = string.Empty;
-}
